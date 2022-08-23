36690f5719278dd1b151188ba6fc4a37e4c07c3b
Akram_Mo <akram.mo.gb@gmail.com>
2022-08-23 06:59:48 +0200

    Almost done with auth

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
+const session = require("express-session");
+const authRouter = require("./routes/auth");
+const cookieParser = require("cookie-parser");
 const { addUser } = require("./models/userDB.js");
 const { findMessages, addMessage } = require("./models/messageDB.js");
-const authRouter = require("./auth");
-const cors = require("cors");
 
 dotenv.config();
 const app = express();
@@ -18,17 +21,44 @@ mongoose.connect(process.env.DATABASE_URL, () =>
     console.log("connected to db")
 );
 
-app.use(express.json(), cors());
-app.use("/auth", authRouter);
+app.use(
+    session({
+        secret: "c3e4088a6b80e22cd4dbfcc63ada01bbbeb31b88463ee9c2cbf86eca268aa355e8e0bb80ddcb7d607d06c20965ac5e53b0fa2bdc8d09079c9ce9f28f76c4e9c1",
+        saveUninitialized: true,
+        resave: true,
+    })
+);
 
-/* app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views/chat"));
+app.use(flash());
+
+app.use(cookieParser());
+app.use(express.json(), cors());
 app.use(express.urlencoded({ extended: false }));
+
+// setting the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views/components"));
+
+// setting public files
 app.use(express.static("public"));
 app.use(express.static("public/js"));
 app.use(express.static("public/css"));
 app.use(express.static("public/assets"));
 
+// setting views per path
+app.use(function (req, res, next) {
+    if (req.path.includes("auth"))
+        app.set("views", path.join(__dirname, "views/auth"));
+    else app.set("views", path.join(__dirname, "views/components"));
+    next();
+});
+
+app.use("/auth", authRouter);
+app.get("/chat", (req, res) => {
+    res.render("chat");
+});
+
+/*
 var counter = 1;
 var usersList = [];
 var temporaryName = `akram ${counter}`;
